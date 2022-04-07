205573f8e1d9164891fe287586b90d7b4d9b3430
Matheus Alves <matheusalves.bmd@gmail.com>
2022-04-07 17:37:32 -0300

    Fix some config mistakes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+const path = require("path");
 
 const app = express();
 
@@ -9,7 +10,20 @@ app.use(express.urlencoded());
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
-app.set("views", "views");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+app.post("/greetings", (req, res, next) => {
+  if (req.params.fname) res.locals.fname = res.params.fname;
+  else res.locals.fname = "";
+  if (req.params.lname) res.locals.lname;
+  else res.locals.lname = "";
+
+  res.render("welcome");
+});
 
 app.get("/test", (req, res, next) => {
   res.render("test");
